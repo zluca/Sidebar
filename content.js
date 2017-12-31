6c99b01a3c0a44d479cb73ce109e74afca5700ab
zluka <osam@tut.by>
2017-12-31 12:33:27 +0300

    iframe hover issues

--- content.js
+++ content.js
@@ -375,6 +375,7 @@ function setSideBarFixed(side, value) {
 		}
 	}
 	else {
+		setHover(side, false);
 		setListener('mouseover', true);
 		setListener('mouseleave', true);
 		setListener('borderclick', false);
