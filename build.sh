1d5574103c6d5cf1eabf8002379690d8fc830c3b
zluka <osam@tut.by>
2019-03-16 11:51:30 +0300

    remove global dependecies

--- build.sh
+++ build.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-# This script requares:
-
-# uglifyjs
-# https://www.npmjs.com/package/uglify-es
-
-# minify
-# https://github.com/tdewolff/minify
-
-# svgo
-# https://www.npmjs.com/package/svgo
-
-# web-ext
-# https://www.npmjs.com/package/web-ext
+n=$(ls ./node_modules | wc -l)
+if [ "$n" -eq '0' ]
+then
+	npm i svgo web-ext uglify-es
+else
+	npm update
+fi
 
 rm -rf ./temp
 
@@ -21,6 +15,7 @@ mkdir ./temp/icons
 mkdir ./builds
 cp -r ./_locales ./temp/
 cp *.html ./temp/
+cp *.css ./temp/
 cp ./icons/*.gif ./temp/icons/
 cp ./icons/*.png ./temp/icons/
 cp LICENSE ./temp/
@@ -32,17 +27,7 @@ while [ "$i" -gt 0 ]
 do
 	jsFile=$(echo "$jsFiles" | sed -n ${i}p)
 	echo $jsFile
-	uglifyjs --ecma 6 -cm -o ./temp/$jsFile $jsFile
-	let 'i -= 1'
-done
-
-cssFiles=$(ls *.css)
-i=$(ls *.css | wc -l)
-while [ "$i" -gt 0 ]
-do
-	cssFile=$(echo "$cssFiles" | sed -n ${i}p)
-	echo $cssFile
-	minify --mime text/css -o ./temp/$cssFile $cssFile
+	./node_modules/uglify-es/bin/uglifyjs --ecma 6 -cm -o ./temp/$jsFile $jsFile
 	let 'i -= 1'
 done
 
@@ -53,14 +38,14 @@ while [ "$i" -gt 0 ]
 do
 	svgFile=$(echo "$svgFiles" | sed -n ${i}p)
 	echo $svgFile
-	svgo -p 0 -i $svgFile -o ../temp/icons/$svgFile
+	../node_modules/svgo/bin/svgo -p 0 -i $svgFile -o ../temp/icons/$svgFile
 	let 'i -= 1'
 done
 
 cd ../temp
 touch manifest.json
 cat ../manifest.json | sed 's/\/\/\ //g' >> manifest.json
-web-ext build
+../node_modules/web-ext/bin/web-ext build
 cd web-ext-artifacts
 extName=$(ls -N *.zip)
 cp -f ./"$extName" ../../builds/firefox-"$extName"
@@ -70,7 +55,7 @@ rm manifest.json
 rm -rf ./web-ext-artifacts
 touch manifest.json
 cat ../manifest.json | sed 's/.*\/\/\ .*//g' >> manifest.json
-web-ext build
+../node_modules/web-ext/bin/web-ext build
 cd web-ext-artifacts
 extName=$(ls -N *.zip)
 cp -f ./"$extName" ../../builds/chromium-"$extName"
